306a7fd46b109431b04bb047d2aa101ec155861b
Aman-Lama311 <lamaaman2036@gmail.com>
2025-03-23 17:26:54 +0545

    updated some code

--- src/pages/home/Footer.jsx
+++ src/pages/home/Footer.jsx
@@ -3,11 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-
-      <svg viewBox="0 0 1440 320"><path fill="black" fill-opacity="1" d="M0,288L1440,32L1440,320L0,320Z"></path>
-
-        <text fill='white' x={700} y={250}>Develop By: Someone</text></svg>
-
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#000" fill-opacity="1" d="M0,224L1440,128L1440,320L0,320Z"></path>
+     <text x={630} y={250} fill='white' fontSize={20}>Developed by: Some Dev</text>
+     </svg>
 
     </div>
   )
