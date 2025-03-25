e49b5a6cd957b410be655cfac3cf307a96f9cad4
Aman-Lama311 <lamaaman2036@gmail.com>
2025-03-25 08:26:34 +0545

    changes

--- src/pages/home/LikeDislike.jsx
+++ src/pages/home/LikeDislike.jsx
@@ -7,7 +7,7 @@ const LikeDislike = () => {
   return (
     <div className='bg-gray-300 w-full h-screen flex justify-center items-center'>
       <div className='p-4 bg-white w-[300px] rounded-md'>
-        <img src="src/assets/ice.png" alt="" />
+        <img src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/6/1/1/FNM_070112-Milky-Way-Ice-Cream-Recipe_s4x3.jpg.rend.hgtvcom.1280.960.suffix/1382541468590.webp" alt="" />
         <div>
           <h1 className='text-lg font-semibold'>Ice Cream</h1>
           <p className='flex gap-2 items-center'><span><AiFillLike size={20} onClick={() => setLike(like + 1)}/></span>
